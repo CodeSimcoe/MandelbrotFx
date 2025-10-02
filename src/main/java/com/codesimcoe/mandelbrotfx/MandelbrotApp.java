7a92049dc58092de2d66dc5c580c57bdbeb551cb
Clement de Tastes <isomorphisme@gmail.com>
2025-10-02 11:16:21 +0200

    App vs Main

--- src/main/java/com/codesimcoe/mandelbrotfx/MandelbrotApp.java
+++ src/main/java/com/codesimcoe/mandelbrotfx/MandelbrotApp.java
@@ -8,11 +8,7 @@ import javafx.stage.Stage;
 import java.io.InputStream;
 import java.util.Objects;
 
-public class MandelbrotMain extends Application {
-
-  public static void main(final String[] args) {
-    launch(args);
-  }
+public class MandelbrotApp extends Application {
 
   @Override
   public void start(final Stage primaryStage) {
