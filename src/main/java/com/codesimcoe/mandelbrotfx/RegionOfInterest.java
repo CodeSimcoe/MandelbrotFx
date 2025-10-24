ef445353d9fc26f1f6e0be39a2ed7e2113c98f30
Clément de Tastes <isomorphisme@gmail.com>
2025-10-24 08:48:21 +0200

    remove value from RegionOfInterest

--- src/main/java/com/codesimcoe/mandelbrotfx/RegionOfInterest.java
+++ src/main/java/com/codesimcoe/mandelbrotfx/RegionOfInterest.java
@@ -1,6 +1,6 @@
 package com.codesimcoe.mandelbrotfx;
 
-public value record RegionOfInterest(String name, Region region, int iterations) implements Named {
+public record RegionOfInterest(String name, Region region, int iterations) implements Named {
   @Override
   public String getName() {
     return this.name;
